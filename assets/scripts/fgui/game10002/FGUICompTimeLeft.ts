0092baf87bda4a43ddc26cf154ba91fff6a2b302
kakamatong <631603920@qq.com>
2026-03-03 09:46:05 +0800

    feat(ui): 添加倒计时组件CompTimeLeft
    - 在主界面布局中调整地图组件位置，新增倒计时组件CompTimeLeft
    - 新增CompTimeLeft组件XML布局文件，包含进度条和时间文本
    - 添加倒计时组件相关图片资源bar.png和barbg.png
    - 更新包资源描述文件，注册倒计时组件及其资源
    - 新增FGUICompTimeLeft TypeScript类，实现组件逻辑及显示隐藏方法

---
+++ assets/scripts/fgui/game10002/FGUICompTimeLeft.ts
@@ -0,0 +1,60 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+import { assetManager, AssetManager } from "cc";
+import * as fgui from "fairygui-cc";
+
+import { PackageManager } from "../../frameworks/PackageManager";
+
+export default class FGUICompTimeLeft extends fgui.GComponent {
+
+	public UI_IMG_BAR:fgui.GImage;
+	public UI_TXT_TIME_MSG:fgui.GTextField;
+	public static URL:string = "ui://2zsfe53xmnt112";
+
+	public static packageName:string = "game10002";
+
+	public static instance:any | null = null;
+
+	public static showView(params?:any, callBack?:(b:boolean)=>void):void {
+		if(FGUICompTimeLeft.instance) {
+			console.log("allready show");
+			callBack&&callBack(false);
+			return;
+		}
+		PackageManager.instance.loadPackage("fgui", this.packageName).then(()=> {
+
+			const view = fgui.UIPackage.createObject("game10002", "CompTimeLeft") as FGUICompTimeLeft;
+
+			view.makeFullScreen();
+			FGUICompTimeLeft.instance = view;
+			fgui.GRoot.inst.addChild(view);
+			view.show && view.show(params);
+			callBack&&callBack(true);
+		}
+		).catch(error=>{console.log("showView error", error);callBack&&callBack(false);return;});
+	}
+
+	protected onDestroy():void {
+		super.onDestroy();
+		FGUICompTimeLeft.instance = null;
+	}
+	public static hideView():void {
+		FGUICompTimeLeft.instance && FGUICompTimeLeft.instance.dispose();
+	}
+
+	show(data?:any):void{};
+
+	public static createInstance():FGUICompTimeLeft {
+		return <FGUICompTimeLeft>(fgui.UIPackage.createObject("game10002", "CompTimeLeft"));
+	}
+
+	protected onConstruct():void {
+		this.UI_IMG_BAR = <fgui.GImage>(this.getChildAt(1));
+		this.UI_TXT_TIME_MSG = <fgui.GTextField>(this.getChildAt(2));
+	}
+	scheduleOnce(callback: () => void, delay: number):void{};
+	unscheduleAllCallbacks():void{};
+	unschedule(callback: () => void):void{};
+	schedule(callback: () => void, interval: number):void{};
+}
+fgui.UIObjectFactory.setExtension(FGUICompTimeLeft.URL, FGUICompTimeLeft);
