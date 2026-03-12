e48e0f6bb9559c60fcb284ff7b94526b87cb0934
kakamatong <631603920@qq.com>
2026-03-12 14:15:31 +0800

    feat(privateRoom): 优化私密房间界面及创建逻辑
    - 调整局数与人数选项，增加10局和6人选项，界面布局优化
    - 修正控制器索引及按钮点击绑定对应关系
    - 修改控件页面索引映射，使用selectedPage替代selectedIndex设置控制器状态
    - 优化创建私密房间请求参数，使用正确的gameid (10002)发送请求
    - 完善创建房间响应处理，支持弹窗提示用户已在游戏中并引导返回
    - 统一字符串引号风格，添加缺失分号规范代码风格
    - 修正显示文本位置，更新“模式”标签文本位置和内容显示

--- assets/scripts/fgui/privateRoom/FGUICompPrivateCreate.ts
+++ assets/scripts/fgui/privateRoom/FGUICompPrivateCreate.ts
@@ -58,13 +58,13 @@ export default class FGUICompPrivateCreate extends fgui.GComponent {
 		this.ctrl_cnt = this.getControllerAt(1);
 		this.UI_BTN_CREATE = <fgui.GButton>(this.getChildAt(1));
 		this.UI_BTN_CREATE.onClick(this.onBtnCreate, this);
-		this.UI_BTN_JU3 = <fgui.GButton>(this.getChildAt(5));
+		this.UI_BTN_JU3 = <fgui.GButton>(this.getChildAt(6));
 		this.UI_BTN_JU3.onClick(this.onBtnJu3, this);
-		this.UI_BTN_JU5 = <fgui.GButton>(this.getChildAt(6));
+		this.UI_BTN_JU5 = <fgui.GButton>(this.getChildAt(7));
 		this.UI_BTN_JU5.onClick(this.onBtnJu5, this);
-		this.UI_BTN_JU7 = <fgui.GButton>(this.getChildAt(7));
+		this.UI_BTN_JU7 = <fgui.GButton>(this.getChildAt(8));
 		this.UI_BTN_JU7.onClick(this.onBtnJu7, this);
-		this.UI_BTN_JU0 = <fgui.GButton>(this.getChildAt(8));
+		this.UI_BTN_JU0 = <fgui.GButton>(this.getChildAt(9));
 		this.UI_BTN_JU0.onClick(this.onBtnJu0, this);
 	}
 	scheduleOnce(callback: () => void, delay: number):void{};
--- assets/scripts/view/privateRoom/comp/CompPrivateCreate.ts
+++ assets/scripts/view/privateRoom/comp/CompPrivateCreate.ts
@@ -23,17 +23,17 @@ import { SprotoCreatePrivateRoom } from "db://assets/types/protocol/lobby/c2s";
 @ViewClass()
 export class CompPrivateCreate extends FGUICompPrivateCreate {
     /** 视图数据 */
-    private _data:any|null = null;
+    private _data: any | null = null;
 
     /**
      * @description 显示创建私密房间界面
      * @param data 视图数据
      */
-    show(data?:any){
+    show(data?: any) {
         this._data = data;
-        const strRule = sys.localStorage.getItem(LOCAL_KEY.PRIVATE_RULE)
-        if (strRule && strRule !== '') {
-            this.initUI(JSON.parse(strRule))
+        const strRule = sys.localStorage.getItem(LOCAL_KEY.PRIVATE_RULE);
+        if (strRule && strRule !== "") {
+            this.initUI(JSON.parse(strRule));
         }
     }
 
@@ -41,16 +41,16 @@ export class CompPrivateCreate extends FGUICompPrivateCreate {
      * @description 初始化UI
      * @param rule 房间规则
      */
-    initUI(rule:any):void{
-        this.ctrl_mode.selectedIndex = rule.mode;
-        this.ctrl_cnt.selectedIndex = CREATE_ROOM_PLAYER_CNT.indexOf(rule.playerCnt);
+    initUI(rule: any): void {
+        this.ctrl_mode.selectedPage = `${rule.playNum}`;
+        this.ctrl_cnt.selectedPage = `${rule.playerCnt}`;
     }
 
     /**
      * @description 关闭按钮点击事件
      */
     onBtnClose(): void {
-        CompPrivateCreate.hideView()
+        CompPrivateCreate.hideView();
     }
 
     /**
@@ -58,38 +58,38 @@ export class CompPrivateCreate extends FGUICompPrivateCreate {
      */
     onBtnCreate(): void {
         const gameRule = {
-            playerCnt:CREATE_ROOM_PLAYER_CNT[this.ctrl_cnt.selectedIndex],
-            mode: this.ctrl_mode.selectedIndex
-        }
-        const func = (result:any)=>{
-            if(result && result.code == 1){
-                ConnectGameSvr.instance.connectGame(result,(b:boolean)=>{
+            playerCnt: Number(this.ctrl_cnt.selectedPage),
+            playNum: Number(this.ctrl_mode.selectedPage),
+        };
+        const func = (result: any) => {
+            if (result && result.code == 1) {
+                ConnectGameSvr.instance.connectGame(result, (b: boolean) => {
                     if (b) {
-                        this._data && (this._data.changeToGameScene && this._data.changeToGameScene())
+                        this._data && this._data.changeToGameScene && this._data.changeToGameScene();
                     }
-                })
-            }else if (result && result.code == 0 && result.gameid > 0) {
+                });
+            } else if (result && result.code == 0 && result.gameid > 0) {
                 PopMessageView.showView({
-                    content:'您已在游戏中,是否返回',
-                    type:ENUM_POP_MESSAGE_TYPE.NUM2,
-                    sureBack:()=>{
-                        ConnectGameSvr.instance.connectGame(result,(b:boolean)=>{
+                    content: "您已在游戏中,是否返回",
+                    type: ENUM_POP_MESSAGE_TYPE.NUM2,
+                    sureBack: () => {
+                        ConnectGameSvr.instance.connectGame(result, (b: boolean) => {
                             if (b) {
-                                this._data && (this._data.changeToGameScene && this._data.changeToGameScene())
+                                this._data && this._data.changeToGameScene && this._data.changeToGameScene();
                             }
-                        })
-                    }
-                })
-            }else{
-                const msg = result && result.msg ? result.msg : '未知错误';
-                TipsView.showView({content:msg})
+                        });
+                    },
+                });
+            } else {
+                const msg = result && result.msg ? result.msg : "未知错误";
+                TipsView.showView({ content: msg });
             }
-        }
+        };
 
-        const strRule = JSON.stringify(gameRule)
-        sys.localStorage.setItem(LOCAL_KEY.PRIVATE_RULE, strRule)
-        const reqData = {gameid:10001, rule:strRule}
-        LobbySocketManager.instance.sendToServer(SprotoCreatePrivateRoom,reqData, func)
+        const strRule = JSON.stringify(gameRule);
+        sys.localStorage.setItem(LOCAL_KEY.PRIVATE_RULE, strRule);
+        const reqData = { gameid: 10002, rule: strRule };
+        LobbySocketManager.instance.sendToServer(SprotoCreatePrivateRoom, reqData, func);
     }
 }
 fgui.UIObjectFactory.setExtension(CompPrivateCreate.URL, CompPrivateCreate);
