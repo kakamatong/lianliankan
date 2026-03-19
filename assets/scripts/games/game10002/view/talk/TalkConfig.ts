2fc31853e7a062d1ab6edd2502875cbb3cb3f800
kakamatong <631603920@qq.com>
2026-03-19 17:56:28 +0800

    refactor(talk): 优化快捷聊天消息列表内容
    - 精简并更新默认聊天消息内容
    - 统一消息对象的格式，增加空格对齐
    - 保持消息顺序和结构更清晰易读

--- assets/scripts/games/game10002/view/talk/TalkConfig.ts
+++ assets/scripts/games/game10002/view/talk/TalkConfig.ts
@@ -9,7 +9,7 @@
  * @description 消息转发类型
  */
 export enum FORWARD_MESSAGE_TYPE {
-    TALK = 1,  // 快捷聊天
+    TALK = 1, // 快捷聊天
 }
 
 /**
@@ -18,15 +18,10 @@ export enum FORWARD_MESSAGE_TYPE {
  * @property {string} msg 聊天内容
  */
 export const TALK_LIST = [
-    {id:1,msg:"我出剪刀，你信吗？"},
-    {id:2,msg:"我出石头，你信吗？"},
-    {id:3,msg:"我出布，你信吗？"},
-    {id:4,msg:"真墨迹！"},
-    {id:5,msg:"我猜你会出剪刀~"},
-    {id:6,msg:"我猜你会出石头~"},
-    {id:7,msg:"我猜你会出布~"},
-    {id:8,msg:"天大地大，石头最大！"},
-    {id:9,msg:"你出剪刀，我出布，这把我让你！"},
-    {id:10,msg:"你出石头，我出剪刀，这把我让你！"},
-    {id:11,msg:"你出布，我出剪刀，这把我让你！"},
-]
+    { id: 1, msg: "准备！准备！" },
+    { id: 2, msg: "加油！" },
+    { id: 3, msg: "真慢~~" },
+    { id: 4, msg: "真墨迹！" },
+    { id: 5, msg: "我闭着眼睛都比你快。" },
+    { id: 6, msg: "这把让你了。" },
+];
