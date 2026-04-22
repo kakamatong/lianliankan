d3a76281085f3e55aad0bf4c1e9202a8ee47e1b5
kakamatong <631603920@qq.com>
2026-04-22 16:26:20 +0800

    feat(fgui): 优化弹窗与用户中心界面组件及样式
    - 调整PopMessageView组件位置，确保显示正确
    - 为CompBtnScale添加颜色控制器及状态对应的颜色样式
    - 新增CompPopBg背景组件并替换旧背景图片popbg.png
    - 更新CompPopMessage组件尺寸、样式与按钮布局，应用新背景组件
    - 删除旧背景图片资源popbg.png，新增popbg2.png及相关组件资源
    - 更新UserCenterView布局，替换背景图组件及调整控件位置和样式
    - 修改CompBtnSound组件尺寸、默认状态及图标和文字颜色
    - 新增FGUICompBtnScale类，支持按钮颜色控制和全屏显示功能
    - 修改FGUICompPopMessage组件引用FGUICompBtnScale以支持颜色控制
    - 调整FGUIUserCenterView控件索引，新增标题控件UI_TXT_TITLE并绑定事件

--- assets/scripts/fgui/userCenter/FGUIUserCenterView.ts
+++ assets/scripts/fgui/userCenter/FGUIUserCenterView.ts
@@ -22,6 +22,7 @@ export default class FGUIUserCenterView extends fgui.GComponent {
 	public UI_BTN_EFFECT:FGUICompBtnSound;
 	public UI_TXT_CP:fgui.GTextField;
 	public UI_BTN_DEL_ACC:fgui.GButton;
+	public UI_TXT_TITLE:fgui.GTextField;
 	public static URL:string = "ui://1zcuqn2bp4e40";
 
 	public static packageName:string = "userCenter";
@@ -63,23 +64,24 @@ export default class FGUIUserCenterView extends fgui.GComponent {
 
 	protected onConstruct():void {
 		this.UI_COMP_HEAD = <fgui.GComponent>(this.getChildAt(3));
-		this.UI_TXT_NICKNAME = <fgui.GTextField>(this.getChildAt(5));
-		this.UI_TXT_USERID = <fgui.GTextField>(this.getChildAt(6));
-		this.UI_BTN_WECHAT = <fgui.GButton>(this.getChildAt(7));
+		this.UI_TXT_NICKNAME = <fgui.GTextField>(this.getChildAt(4));
+		this.UI_TXT_USERID = <fgui.GTextField>(this.getChildAt(5));
+		this.UI_BTN_WECHAT = <fgui.GButton>(this.getChildAt(6));
 		this.UI_BTN_WECHAT.onClick(this.onBtnWechat, this);
-		this.UI_TXT_WIN = <fgui.GTextField>(this.getChildAt(12));
-		this.UI_TXT_LOSE = <fgui.GTextField>(this.getChildAt(13));
-		this.UI_TXT_DRAW = <fgui.GTextField>(this.getChildAt(14));
-		this.UI_TXT_RATE = <fgui.GTextField>(this.getChildAt(15));
-		this.UI_BTN_CLOSE = <fgui.GButton>(this.getChildAt(16));
+		this.UI_TXT_WIN = <fgui.GTextField>(this.getChildAt(11));
+		this.UI_TXT_LOSE = <fgui.GTextField>(this.getChildAt(12));
+		this.UI_TXT_DRAW = <fgui.GTextField>(this.getChildAt(13));
+		this.UI_TXT_RATE = <fgui.GTextField>(this.getChildAt(14));
+		this.UI_BTN_CLOSE = <fgui.GButton>(this.getChildAt(15));
 		this.UI_BTN_CLOSE.onClick(this.onBtnClose, this);
-		this.UI_BTN_BGMUSIC = <FGUICompBtnSound>(this.getChildAt(19));
+		this.UI_BTN_BGMUSIC = <FGUICompBtnSound>(this.getChildAt(18));
 		this.UI_BTN_BGMUSIC.onClick(this.onBtnBgmusic, this);
-		this.UI_BTN_EFFECT = <FGUICompBtnSound>(this.getChildAt(20));
+		this.UI_BTN_EFFECT = <FGUICompBtnSound>(this.getChildAt(19));
 		this.UI_BTN_EFFECT.onClick(this.onBtnEffect, this);
-		this.UI_TXT_CP = <fgui.GTextField>(this.getChildAt(22));
-		this.UI_BTN_DEL_ACC = <fgui.GButton>(this.getChildAt(23));
+		this.UI_TXT_CP = <fgui.GTextField>(this.getChildAt(21));
+		this.UI_BTN_DEL_ACC = <fgui.GButton>(this.getChildAt(22));
 		this.UI_BTN_DEL_ACC.onClick(this.onBtnDelAcc, this);
+		this.UI_TXT_TITLE = <fgui.GTextField>(this.getChildAt(23));
 	}
 	scheduleOnce(callback: () => void, delay: number):void{};
 	unscheduleAllCallbacks():void{};
