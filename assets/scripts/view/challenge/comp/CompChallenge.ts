6a84103c857990e7393a2704f80f58540146162b
kakamatong <631603920@qq.com>
2026-06-10 15:49:37 +0800

    feat(challenge): 更新挑战组件，初始化用户能量请求并修改能量显示文本

--- assets/scripts/view/challenge/comp/CompChallenge.ts
+++ assets/scripts/view/challenge/comp/CompChallenge.ts
@@ -2,12 +2,18 @@ import FGUICompChallenge from "@fgui/challenge/FGUICompChallenge";
 import { ViewClass } from "@frameworks/Framework";
 import * as fgui from "fairygui-cc";
 import { ChallengeView } from "../ChallengeView";
+import { UserEnergy } from "@modules/UserEnergy";
 
 @ViewClass({ curveScreenAdapt: true })
 export class CompChallenge extends FGUICompChallenge {
     onConstruct() {
         super.onConstruct();
+        this.init();
         this.show();
+    }
+
+    init() {
+        UserEnergy.instance.req();
     }
 
     onDestroy() {
