b453a9d55c56fdbdb4c621f9510793868dec0aa4
kakamatong <631603920@qq.com>
2026-06-16 20:54:59 +0800

    feat(CompChallenge): 添加获取闯关配置的逻辑，增强模块功能

--- assets/scripts/view/challenge/comp/CompChallenge.ts
+++ assets/scripts/view/challenge/comp/CompChallenge.ts
@@ -3,6 +3,7 @@ import { ViewClass } from "@frameworks/Framework";
 import * as fgui from "fairygui-cc";
 import { ChallengeView } from "../ChallengeView";
 import { UserEnergy } from "@modules/UserEnergy";
+import { Challenge } from "@modules/Challenge";
 
 @ViewClass({ curveScreenAdapt: true })
 export class CompChallenge extends FGUICompChallenge {
@@ -14,6 +15,13 @@ export class CompChallenge extends FGUICompChallenge {
 
     init() {
         UserEnergy.instance.req();
+        Challenge.instance.getConfig((success, data) => {
+            if (success) {
+                console.log("闯关配置获取成功");
+            } else {
+                console.warn("闯关配置获取失败");
+            }
+        });
     }
 
     onDestroy() {
