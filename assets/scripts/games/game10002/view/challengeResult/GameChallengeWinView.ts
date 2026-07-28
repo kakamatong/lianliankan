840684b49871993aa3bd00503778be47b8125ccf
kakamatong <631603920@qq.com>
2026-07-28 10:57:44 +0800

    feat: 更新资源文件和挑战赛胜利视图，优化加载逻辑

--- assets/scripts/games/game10002/view/challengeResult/GameChallengeWinView.ts
+++ assets/scripts/games/game10002/view/challengeResult/GameChallengeWinView.ts
@@ -13,7 +13,7 @@ import { PackageLoad, ViewClass } from "@frameworks/Framework";
  * @description 挑战赛胜利视图，继承自 FGUI 自动生成的 FGUIGameChallengeWinView
  * @category 游戏 10002 - 连连看
  */
-@PackageLoad(["gameChallengeResult"])
+@PackageLoad(["gameCommon", "gameChallengeResult"])
 @ViewClass()
 export class GameChallengeWinView extends FGUIGameChallengeWinView {}
 
