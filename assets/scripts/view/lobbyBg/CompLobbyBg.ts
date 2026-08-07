d18efdd84a2c5f694fe7f972941076415d372269
kakamatong <631603920@qq.com>
2026-08-07 16:15:58 +0800

    feat: 新增大厅背景入口组件，启动背景动画

---
+++ assets/scripts/view/lobbyBg/CompLobbyBg.ts
@@ -0,0 +1,25 @@
+/**
+ * @file CompLobbyBg.ts
+ * @description 大厅背景入口组件：负责启动方块页动画
+ * @category 大厅背景
+ */
+
+import * as fgui from "fairygui-cc";
+import FGUICompLobbyBg from "@fgui/lobbyBg/FGUICompLobbyBg";
+import { PackageLoad, ViewClass } from "@frameworks/Framework";
+import { CompBgCubePage } from "./comp/CompBgCubePage";
+
+/**
+ * @class CompLobbyBg
+ * @description 大厅背景入口，控制背景动画的启停
+ * @category 大厅背景
+ */
+@PackageLoad(["lobbyBg"])
+@ViewClass()
+export class CompLobbyBg extends FGUICompLobbyBg {
+    onConstruct() {
+        super.onConstruct();
+        (this.UI_COMP_CUBE_PAGE as CompBgCubePage).startAnimation();
+    }
+}
+fgui.UIObjectFactory.setExtension(CompLobbyBg.URL, CompLobbyBg);
