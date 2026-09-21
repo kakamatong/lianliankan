7f2bbe033b1d1f2c1dafdf7910c2627d7cdcfd8c
kakamatong <631603920@qq.com>
2026-09-21 21:23:29 +0800

    feat: 更新排行榜文本，调整组件位置，新增按钮功能及相关文件

---
+++ assets/scripts/fgui/common/FGUICompBtnSingleCheck.ts
@@ -0,0 +1,97 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+import { assetManager, AssetManager } from "cc";
+import * as fgui from "fairygui-cc";
+
+import { PackageManager } from "@frameworks/PackageManager";
+import { Logger } from "@frameworks/utils/Utils";
+
+export default class FGUICompBtnSingleCheck extends fgui.GButton {
+
+	public ctrl_color:fgui.Controller;
+	public static URL:string = "ui://gj0r6g5inomuz";
+
+	public static packageName:string = "common";
+
+	public static instance:any | null = null;
+
+	public enableAnimation: boolean = false;
+
+	public static showView(params?: any, callBack?: (b: boolean) => void): void {
+		if (FGUICompBtnSingleCheck.instance) {
+			console.log("allready show");
+			callBack && callBack(false);
+			return;
+		}
+		const createView = () => {
+			const view = fgui.UIPackage.createObject("common", "CompBtnSingleCheck") as FGUICompBtnSingleCheck;
+
+			view.makeFullScreen();
+			FGUICompBtnSingleCheck.instance = view;
+			fgui.GRoot.inst.addChild(view);
+			view.show && view.show(params);
+			callBack && callBack(true);
+		};
+
+		if (PackageManager.instance.hasPackage("fgui", this.packageName)) {
+			createView();
+			return;
+		}
+
+		PackageManager.instance
+			.loadPackage("fgui", this.packageName)
+			.then(() => {
+				createView();
+			})
+			.catch((error) => {
+				Logger.error("showView error", error);
+				callBack && callBack(false);
+				return;
+			});
+	}
+
+	protected onDestroy():void {
+		super.onDestroy();
+		FGUICompBtnSingleCheck.instance = null;
+	}
+	public static hideView():void {
+		FGUICompBtnSingleCheck.instance && FGUICompBtnSingleCheck.instance.dispose();
+	}
+
+	show(data?:any):void{};
+
+	enterAnimation(): void {
+		fgui.GTween.to2(0, 0, 1, 1, 0.3)
+		    .setTarget(this)
+		    .setEase(fgui.EaseType.BackOut)
+		    .onUpdate((tween) => {
+		        this.setScale(tween.value.x, tween.value.y);
+		    });
+	}
+
+	hideAnimation(onComplete?: () => void): void {
+		fgui.GTween.to2(1, 1, 0, 0, 0.3)
+		    .setTarget(this)
+		    .setEase(fgui.EaseType.BackIn)
+		    .onUpdate((tween) => {
+		        this.setScale(tween.value.x, tween.value.y);
+		    })
+		    .onComplete(() => {
+		        onComplete && onComplete();
+		    });
+	}
+
+	public static createInstance():FGUICompBtnSingleCheck {
+		return <FGUICompBtnSingleCheck>(fgui.UIPackage.createObject("common", "CompBtnSingleCheck"));
+	}
+
+	protected onConstruct():void {
+		this.ctrl_color = this.getControllerAt(1);
+		if (this.enableAnimation) this.enterAnimation();
+	}
+	scheduleOnce(callback: () => void, delay: number):void{};
+	unscheduleAllCallbacks():void{};
+	unschedule(callback: () => void):void{};
+	schedule(callback: () => void, interval: number):void{};
+}
+fgui.UIObjectFactory.setExtension(FGUICompBtnSingleCheck.URL, FGUICompBtnSingleCheck);
